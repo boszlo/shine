2c5492a40870e8ab181251936fe15c7fc1de626c
boszlo <maciej.x.deptula@gmail.com>
2016-12-19 21:46:19 +0000

    lower indexes, search styled and paginated

--- db/structure.sql
+++ db/structure.sql
@@ -180,6 +180,27 @@ ALTER TABLE ONLY users
 
 
 --
+-- Name: customers_lower_email; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX customers_lower_email ON customers USING btree (lower((email)::text));
+
+
+--
+-- Name: customers_lower_first_name; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX customers_lower_first_name ON customers USING btree (lower((first_name)::text) varchar_pattern_ops);
+
+
+--
+-- Name: customers_lower_last_name; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX customers_lower_last_name ON customers USING btree (lower((last_name)::text) varchar_pattern_ops);
+
+
+--
 -- Name: index_customers_on_email; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -220,6 +241,6 @@ CREATE UNIQUE INDEX index_users_on_reset_password_token ON users USING btree (re
 
 SET search_path TO "$user", public;
 
-INSERT INTO schema_migrations (version) VALUES ('20161217164904'), ('20161218122316'), ('20161218130612');
+INSERT INTO schema_migrations (version) VALUES ('20161217164904'), ('20161218122316'), ('20161218130612'), ('20161219211015');
 
 
